cd7b79f0e3c2288120bc10518b38b50c78e92903
MacOne <macone@wnpgmb1009w-ds01-161-18-102.dynamic.bellmts.net>
2023-09-10 18:02:46 +0500

    initial codes

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <div class="container">
+
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Navbar from './components/TheNavbar.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Navbar
+  }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+a {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="navbar">
+    <span class="logo">BookSearch</span>
+    <ul class="nav-links">
+      <li><a href="/">register</a></li>
+      <li><a href="/">login</a></li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Navbar",
+ 
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
